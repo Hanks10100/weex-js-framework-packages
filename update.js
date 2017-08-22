32f5ff7b7f989688009971d18100321673d4b8c0
Hanks <zhanghan.me@gmail.com>
2017-08-22 13:28:26 +0800

    add versions

--- update.js
+++ update.js
@@ -1,7 +1,12 @@
 const fs = require('fs')
 const semver = require('semver')
+const jsonfile = require('jsonfile')
 const child_process = require('child_process')
-const pkg = require('./package.json')
+const versions = require('./versions.json')
+
+function isInstalled (version) {
+  return versions.some(v => semver.satisfies(version, v))
+}
 
 function exec (command) {
   const tasks = Array.isArray(command) ? command : [command]
@@ -16,13 +21,12 @@ function exec (command) {
 let targetVersion = process.argv[2]
 let isLatest = false
 
-if (!semver.valid(targetVersion)) {
+if (!targetVersion || targetVersion === 'latest') {
   targetVersion = exec(`npm view weex-js-framework version`).trim()
   isLatest = true
 }
 
-const formerVersion = pkg.devDependencies['weex-js-framework']
-if (formerVersion === targetVersion) {
+if (isInstalled(targetVersion)) {
   console.log(` => weex-js-framework@${targetVersion} is already installed.`)
   process.exit()
 }
@@ -31,21 +35,28 @@ exec([
   `npm install weex-js-framework@${targetVersion} -E`
 ])
 
-const filePath = `v${semver.major(targetVersion)}.${semver.minor(targetVersion)}`
+const filePath = `${semver.major(targetVersion)}.${semver.minor(targetVersion)}`
 if (!fs.existsSync(filePath)) {
   fs.mkdirSync(filePath)
 }
 
 exec([
-  `cp node_modules/weex-js-framework/index.js ${filePath}/${targetVersion}.js`
+  `cp node_modules/weex-js-framework/index.js ${filePath}/${targetVersion}.js`,
+  `cp node_modules/weex-js-framework/index.min.js ${filePath}/${targetVersion}.min.js`
 ])
 
 if (isLatest) {
   exec([
     `cp node_modules/weex-js-framework/index.js ${filePath}/index.js`,
-    `cp node_modules/weex-js-framework/index.js index.js`
+    `cp node_modules/weex-js-framework/index.min.js ${filePath}/index.min.js`,
+    `cp node_modules/weex-js-framework/index.js index.js`,
+    `cp node_modules/weex-js-framework/index.min.js index.min.js`
   ])
 }
+
+versions.push(targetVersion)
+versions.sort((a, b) => semver.gt(a, b) ? 1 : -1)
+jsonfile.writeFileSync('./versions.json', versions, { spaces: 2 })
 
 exec([
   `git add -A`,
